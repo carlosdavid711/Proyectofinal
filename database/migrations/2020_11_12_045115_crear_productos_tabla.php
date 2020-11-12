2839c1d398b3ebd69b1162198fa0c1ab949df0ed
carlosdavid711 <54157158+carlosdavid711@users.noreply.github.com>
2020-11-12 01:00:50 -0600

    version1

---
+++ database/migrations/2020_11_12_045115_crear_productos_tabla.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearProductosTabla extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    Schema::create('productos', function (Blueprint $table)
+      $table->id();
+      $table->string('nombre');
+       $table->float('precio');
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
